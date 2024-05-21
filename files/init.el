9378158e00101dd638db73fc91e51c12b15b7e53
Mathew Taylor <mjtaylor@lavabit.com>
2024-05-21 09:47:28 +0100

    Adds missing parenthesis in init.el

--- files/init.el
+++ files/init.el
@@ -74,4 +74,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hack Nerd Font" :foundry "SRC" :slant normal :weight regular :height 120 :width normal)))))
+ '(default ((t (:family "Hack Nerd Font" :foundry "SRC" :slant normal :weight regular :height 110 :width normal))))))
